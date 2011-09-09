005b1d53e4cedb8b4489a6e54c361e5474f31ea8
Shaun Sephton <shaunsephton@gmail.com>
2011-09-09 12:12:38 +0200

    create default site

--- test_settings.py
+++ test_settings.py
@@ -3,7 +3,10 @@ DATABASE_ENGINE = 'sqlite3'
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sites',
     'snippetscream',
 ]
 
 ROOT_URLCONF = 'snippetscream.tests.urls'
+
+CREATE_DEFAULT_SITE = True
